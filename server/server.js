653f4d763d8ec273a94f1b4ea2d2b82465ae2d38
Sanjana Mahmood <74899722+smahmood00@users.noreply.github.com>
2022-05-15 02:01:56 +1030

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,38 +1,58 @@
-const express =require('express');
-const cors=require('cors');
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
+const cors = require('cors');
+const adminRefresh = require('./refreshData');
+const getLocations = require("./userLocationData");
+const adminLocationData = require("./adminLocationData");
+//////////////////////////////////////////////////////
 const locFunctions=require('./FavouritesAndSearch.js');
 const signInandUp=require('./signIn.js');
 
-const mongoose = require('mongoose'); 
+const app = express()
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+app.use(express.json());
 
-
-mongoose.connect('mongodb+srv://stu110:p869132-@csci2720.m2qbq.mongodb.net/stu110');
-
-const db = mongoose.connection;
-// Upon connection failure
-db.on('error', console.error.bind(console,'Connection error:'));
-// Upon opening the database successfully
-db.once('open', function () {
-console.log("Connection is open...");
+mongoose.connect("mongodb+srv://stu096:p411402-@csci2720.m2qbq.mongodb.net/stu096", (err) => {
+  if (err) {
+    console.log("database encountered the following error: %s", err);
+  } else {
+    console.log("Connected to the Database");
+  }
 });
 
 
 
 
-//const auth= require('./auth.routes');
-const app=express();
-app.use(cors());
-app.use(express.json());
+
+// logout for admin and user
+app.get('/logout', (req, res) => {
+  res.clearCookie("user");
+  res.status(200).json({
+    success: true,
+    redirectUrl: '/login'
+  })
+});
 
 
+//Admin refresh data
+app.post("/newLocation", adminLocationData.register)
+app.get('/refreshData', adminRefresh.refreshData)
+app.get('/locations', getLocations.getAllLocations)
+app.post('/location/:name', getLocations.getLocation)
+
+//Functions in signIn File
 app.post('/signin-user/:role',signInandUp.signIn);
 app.post('/signup-user',signInandUp.signUp);
 
+//Functions in FavouritesAndSearch File
 app.post('/keywordlocation',locFunctions.keywordLocation);
 app.get('/favourites/:name/:loc',locFunctions.addFavourites);
 app.get('/favourites/:userName',locFunctions.listFavourites);
 
-app.listen(5000,()=>{
-    console.log("your application is running");
-
+app.listen(5000, () => {
+  console.log(`Connected to http://localhost:5000`)
 })
