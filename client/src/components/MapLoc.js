50435276b73f532fe561e5280cc624dbe97ccd27
rb-03 <68503150+rb-03@users.noreply.github.com>
2022-05-16 03:50:56 +0800

    Update MapLoc.js

--- client/src/components/MapLoc.js
+++ client/src/components/MapLoc.js
@@ -16,14 +16,22 @@ const MapLoc = () => {
 
     let { name } = useParams()
     const [marker, setMarkers] = useState(null)
+    const [check, setChecker] = useState('')
 
     useEffect(() => {
         fetch(`/location/${name}`, { method: 'GET' })
             .then(response => response.json())
             .then(data => setMarkers(data.response))
     }, [name])
+    
+    useEffect(() => {
+        fetch(`/location/${name}`, { method: 'GET' })
+            .then(response => response.json())
+            .then(data => setChecker(data.response))
+    }, [name])
  
     return (
+        <div>
         <LoadScript
             googleMapsApiKey="AIzaSyDqqZNAZtnVcfFHcIA6bf_icnltZW5op-0"
         >
@@ -38,6 +46,57 @@ const MapLoc = () => {
                 <></>
             </GoogleMap>
         </LoadScript>
+    <div className = "display">
+            <table className="table table-dark">
+                <tbody>
+                <tr > 
+                    <th> Location </th>
+                    <th> Attribute Value</th>
+                </tr>
+                <tr >
+                    <td> Name </td>
+                    <td> {check.name} </td>
+                </tr>
+                <tr >
+                    <td> Latitude </td>
+                    <td> {check.latitude} </td>
+                </tr>
+                <tr >
+                    <td> Longitude </td>
+                    <td> {check.longitude} </td>
+                </tr>
+                <tr>
+                    <td> Humidity </td>
+                    <td> {check.humidity} </td>
+                </tr>
+                <tr>
+                    <td> Precipitation </td>
+                    <td> {check.precip_mm} </td>
+                </tr>
+                <tr>
+                    <td> Temperature </td>
+                    <td> {check.temp_c}</td>
+                </tr>
+                <tr>
+                    <td> Visibility </td>
+                    <td> {check.vis_km} </td>
+                </tr>
+                <tr>
+                    <td> Wind Direction </td>
+                    <td> {check.wind_dir} </td>
+                </tr>
+                <tr>
+                    <td> Wind Speed </td>
+                    <td> {check.wind_kph}</td>
+                </tr>
+                <tr>
+                    <td> Last Update of Data </td>
+                    <td> {check.last_updated} </td>
+                </tr>
+                </tbody>
+            </table>
+            </div> 
+    </div>
     )
 }
 
