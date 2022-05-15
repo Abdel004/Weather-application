e010c90f2ef8caf59aa0e2d726d2359b15a28cdc
rb-03 <68503150+rb-03@users.noreply.github.com>
2022-05-16 01:52:11 +0800

    Admin Components

---
+++ client/src/Admin/src/Request.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Home extends React.Component {
+   
+   render() {
+      return (
+      <div>
+          <h2>Fetch details</h2>
+          <h3>Fetch user location details for Admin.</h3>
+        </div>
+      );
+   }
+}
+
+export default Home;
