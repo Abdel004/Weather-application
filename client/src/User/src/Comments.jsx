bdf2b8c950c88c2b71f58acfc509a98e62e145e0
rb-03 <68503150+rb-03@users.noreply.github.com>
2022-05-16 01:48:59 +0800

    Different components for each file

---
+++ client/src/User/src/Comments.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Comment extends React.Component {
+   render() {
+      return (
+      <div>
+          <h2>Comments</h2>
+          <h3>Users to see and leave comments about the location.</h3>
+        </div>
+      );
+   }
+}
+
+export default Comment;
---
+++ client/src/User/src/Info.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Info extends React.Component {
+   render() {
+      return (
+      <div>
+          <h2>Information</h2>
+          <h3>This gives detailed information about the chosen location of a user.</h3>
+          <p>Includes information shairng through an individual map and comment sharing.</p>
+        </div>
+      );
+   }
+}
+
+export default Info;
