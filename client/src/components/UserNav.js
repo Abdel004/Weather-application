de1b990479b7d8dd4054b2ed5a11b3215912dd11
rb-03 <68503150+rb-03@users.noreply.github.com>
2022-05-16 08:42:08 +0800

    Update UserNav.js

--- client/src/components/UserNav.js
+++ client/src/components/UserNav.js
@@ -9,7 +9,7 @@ const UserNav = (props) => {
             <h1 className="check">Weather API <i className='fa-solid fa-cloud' /> </h1>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <ul className="list-inline">
-                    <li className="list-inline-item" style={{ width: "345x", textAlign: "center" }} ><Link to="/locations">Location Details</Link></li>
+                    <li className="list-inline-item" style={{ width: "345px", textAlign: "center" }} ><Link to="/locations">Location Details</Link></li>
                     <li className="list-inline-item" style={{ width: "345px", textAlign: "center" }}><Link to="/map">Locations map</Link></li>
                     <li className="list-inline-item" style={{ width: "345px", textAlign: "center" }}><Link to="/fav">Favourites</Link></li>
                     <li className="list-inline-item" style={{ width: "345px", textAlign: "center" }}><Link to="/search">location Search</Link></li>
