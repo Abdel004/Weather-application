f629a4cd28b3ca8797a2754471f34431132331c5
rb-03 <68503150+rb-03@users.noreply.github.com>
2022-05-16 08:36:32 +0800

    Update UserNav.js

--- client/src/components/UserNav.js
+++ client/src/components/UserNav.js
@@ -5,14 +5,16 @@ const UserNav = (props) => {
 
     return (
         <div id="menuwrapper">
-            <h4 className="check" style={{ textAlign: "right" }}> {props.username} | <button onClick={() => window.location.href = '/logout'}>Logout </button></h4>
+            <h4 className="check" style={{ textAlign: "right" }}> {props.username} | <button className="btn btn-outline-light btn-lg" onClick={() => window.location.href = '/logout'}>Logout </button></h4>
             <h1 className="check">Weather API <i className='fa-solid fa-cloud' /> </h1>
-            <ul className="check">
-                <li><Link to="/locations">Location Details</Link></li>
-                <li><Link to="/map">Locations map</Link></li>
-                <li><Link to="/fav">Favourites</Link></li>
-                <li><Link to="/search">location Search</Link></li>
-            </ul>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <ul className="list-inline">
+                    <li className="list-inline-item" style={{ width: "345x", textAlign: "center" }} ><Link to="/locations">Location Details</Link></li>
+                    <li className="list-inline-item" style={{ width: "345px", textAlign: "center" }}><Link to="/map">Locations map</Link></li>
+                    <li className="list-inline-item" style={{ width: "345px", textAlign: "center" }}><Link to="/fav">Favourites</Link></li>
+                    <li className="list-inline-item" style={{ width: "345px", textAlign: "center" }}><Link to="/search">location Search</Link></li>
+                </ul>
+            </nav>
             <div className="content">
                 {props.children}
             </div>
