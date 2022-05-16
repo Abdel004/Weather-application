bd3d9b5179c213c39d8b27311dd169ae07090e97
rb-03 <68503150+rb-03@users.noreply.github.com>
2022-05-16 08:18:31 +0800

    Update AdminNav.js

--- client/src/components/AdminNav.js
+++ client/src/components/AdminNav.js
@@ -9,13 +9,15 @@ const UserNav = (props) => {
 
     return (
         <div id="menuwrapper">
-        <h4 className="check" style={{ textAlign: "right" }}> {props.username} | <button onClick={handleClick}>Logout </button></h4>
+        <h4 className="check" style={{ textAlign: "right" }}> {props.username} | <button onClick={handleClick} className="btn btn-outline-light btn-lg">Logout </button></h4>
         <h1 className="check">Weather API <i className='fa-solid fa-cloud' /> </h1>
-        <ul className="check">
-          <li><Link to="/requests" >Data Refresh</Link></li>
-          <li><Link to="/user" >User CRUD</Link></li>
-          <li><Link to="/loc">Location CRUD</Link></li>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <ul className="list-inline">
+          <li className="list-inline-item" style={{ width: "465px", textAlign: "center" }}><Link to="/requests" >Data Refresh</Link></li>
+          <li className="list-inline-item " style={{ width: "465px",textAlign: "center" }}><Link to="/user" >User CRUD</Link></li>
+          <li className="list-inline-item " style={{ width: "465px",textAlign: "center"  }}><Link to="/loc">Location CRUD</Link></li>
         </ul>
+        </nav>
         <div className="content">
           {props.children}
         </div>
